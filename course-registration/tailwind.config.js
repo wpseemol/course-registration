7dcfcf140af4ed39301d03b5624e1efc48ef9499
wpseemol <seemol.contact@gmail.com>
2023-09-14 21:18:31 +0600

    daisyUi install tailwind.config.js file theme generator color set

--- course-registration/tailwind.config.js
+++ course-registration/tailwind.config.js
@@ -5,4 +5,23 @@ export default {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    darkTheme: "light", // name of one of the included themes for dark mode
+
+    themes: [
+      {
+        mytheme: {
+          primary: "#ff9787",
+          secondary: "#35ba8e",
+          accent: "#bf4648",
+          neutral: "#19161d",
+          "base-100": "#F3F3F3", //body background color
+          info: "#7fd3e6",
+          success: "#15c16b",
+          warning: "#efb034",
+          error: "#f32420",
+        },
+      },
+    ],
+  },
 };
