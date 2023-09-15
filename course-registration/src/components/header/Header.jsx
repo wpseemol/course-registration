e2d8c216e612f8f8eb1813d411b301ac7833118d
wpseemol <seemol.contact@gmail.com>
2023-09-15 23:17:15 +0600

    header Course Regis creat home link

--- course-registration/src/components/header/Header.jsx
+++ course-registration/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
     // defult tailwind some class 'sm:mx-8 sm:mt-6 lg:mx-customMargin mx-3 mt-2'
     <header className="sm:mx-8 sm:mt-6 lg:mx-customMargin mx-customMargin">
       <div className="text-center text-headFontSize font-bold">
-        Course Registration
+        <a href="./">Course Registration</a>
       </div>
     </header>
   );
